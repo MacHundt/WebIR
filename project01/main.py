db03759307b11dd51ba8254fa51f4222bfafca6a
Lukas Eichkorn <lukas.eichkorn@uni-konstanz.de>
2015-11-17 14:38:11 +0100

    Fixed an error in the last commit

--- project01/main.py
+++ project01/main.py
@@ -7,10 +7,8 @@ __author__ = 'wikipedia_project_group'
 
 
 def main():
-    """
-    The main function of the project.
-    """
-    geo_location = sretrieve_geo_location('80.220.78.90')
+    """The main function of the project."""
+    geo_location = retrieve_geo_location('80.220.78.90')
 
     if geo_location is not None:
         print("Country: " + geo_location.country.name + " - City: " + geo_location.city.name)

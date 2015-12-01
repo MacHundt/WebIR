f310b8164d05ef002fa8323798c460388a13c5fd
Lukas Eichkorn <lukas.eichkorn@uni-konstanz.de>
2015-12-01 14:08:39 +0100

    Modified the similarity function of writing styles, edited the tagging-process and much much more

--- project01/utils/writing_style_analyzer.py
+++ project01/utils/writing_style_analyzer.py
@@ -3,9 +3,10 @@
 The file used for the writing style analysis.
 """
 
-from writing_styles import WritingStyle, GeolocatedWritingStyle, get_difference
+from writing_styles import WritingStyle, GeolocatedWritingStyle, get_similarity
 
 import pickle
+import os.path
 from edit_extractor import Page, Revision
 
 __author__ = 'wikipedia_project_group'
@@ -93,21 +94,29 @@ class WritingStylePredictor:
 
 
 def main():
-    page = pickle.load(open("../data/pickle/Anarchism", 'rb'))
-    processor = WritingStyleProcessor()
+    if not os.path.isfile("../data/pickle/trainingsset"):
+        page = pickle.load(open("../data/pickle/Anarchism", 'rb'))
+        processor = WritingStyleProcessor()
 
-    processor.process_wikipedia_page(page)
-    for gl_writing_style in processor.writing_style_learner.geo_located_writing_styles:
+        processor.process_wikipedia_page(page)
+        geo_located_writing_styles = processor.writing_style_learner.geo_located_writing_styles
+
+        pickle.dump(geo_located_writing_styles, open("../data/pickle/trainingsset", 'wb'))
+    else:
+        geo_located_writing_styles = pickle.load(open("../data/pickle/trainingsset", 'rb'))
+
+    writing_style = WritingStyle("this article uses both autistic people or autistic person and people with autism see section below in sociology for terminology. english language on the surface individuals who have autism are physically indistinguishable from those without. sometimes autism cooccurs with other disorders and in those cases outward differences may be apparent. some iew.pdf retrieved ::noted behaviours typically developing infants are social beings  early in life they gaze at people turn toward voices grasp at fingers and smile. in contrast most autistic children do not show special interest in faces and seem to have tremendous difficulty learning to engage in everyday human interaction. even in the first few months of life many autistic children seem indifferent to other people lacking the eye contact and interaction with others that non autistic children exhibit naturally. some infants with autism may appear very calm they may cry less often because they do not seek parental attention or ministration. for other children with autism infantile development progresses normally through language acquisition. between  months and  years however skills previously mastered disappear including language and social skills. it has been noted that members of prefer person page  february autistic adults at the autism society of america  conference felt that the term individuals with autism separates their autism from who they are. in other words they believe their autism is part of who they are and want to be called autistic adults.", None)
+    for gl_writing_style in geo_located_writing_styles:
+        similarity = get_similarity(gl_writing_style, writing_style)
+
         print("-" * 20)
         print("Geolocation: " + gl_writing_style.geo_location)
         print("Revision-Count: " + str(gl_writing_style.count))
-        print("Mean-average word length: " + str(gl_writing_style.mean_average_word_length))
-        print("Mean-average word length stdev: " + str(gl_writing_style.mean_stdev_word_length))
-        print("Mean-average sentence length: " + str(gl_writing_style.mean_average_sentence_length))
-        print("Mean-average sentence length: stdev: " + str(gl_writing_style.mean_stdev_sentence_length))
+        print("Similarity standard-deviation word-length: " + '%.2f' % similarity[0] + '%')
+        print("Similarity standard-deviation sentence-length: " + '%.2f' % similarity[1] + '%')
+        print("Similarity tag-counts: " + '%.2f' % similarity[2] + '%')
         print("-" * 20)
         print()
-
 
 if __name__ == '__main__':
     main()
--- project01/utils/writing_styles.py
+++ project01/utils/writing_styles.py
@@ -5,33 +5,44 @@ Uses writing_style (to install: 'pip install writing_style')
 Uses nltk (to install: 'pip install nltk' and nltk.download('punkt') and nltk.download('averaged_perceptron_tagger')
 """
 
-from writing_style.analyzer import average_word_lengths, stdev_word_lengths, average_sentence_lengths,\
-    stdev_sentence_lengths
+from writing_style.analyzer import stdev_word_lengths, stdev_sentence_lengths
 
 import nltk
 from collections import Counter
+from copy import deepcopy
 
 __author__ = 'wikipedia_project_group'
 
 
-def get_difference(gl_writing_style, writing_style):
+def get_similarity(gl_writing_style, writing_style):
     """
-    Gets the percentual difference between a geo-located and normal writing style.
+    Gets the percentual similarity between a geo-located and normal writing style.
     :param gl_writing_style: The geo-located writing style
     :param writing_style: The writing style
-    :return: The difference in percentage
+    :return: The similarity in percentage as a list
     """
-    return [float(gl_writing_style.average_word_length - writing_style.average_word_length) /
-            gl_writing_style.average_word_length,
+    gl_mean_tags = gl_writing_style.get_mean_tags()
+    tag_differences = deepcopy(writing_style.tag_counts)
 
-            float(gl_writing_style.stdev_word_length - writing_style.stdev_word_length) /
-            gl_writing_style.stdev_word_length,
+    for key, value in writing_style.tag_counts.items():
+        if gl_mean_tags[key] == 0:
+            tag_differences[key] = 0.0
+        else:
+            tag_differences[key] = (gl_mean_tags[key] - float(value)) / gl_mean_tags[key]
 
-            float(gl_writing_style.average_sentence_length - writing_style.average_sentence_length) /
-            gl_writing_style.average_sentence_length,
+    match = 0.0
+    for _, value in tag_differences.items():
+        match += 1 - abs(value)
 
-            float(gl_writing_style.stdev_sentence_length - writing_style.stdev_sentence_length) /
-            gl_writing_style.stdev_sentence_length]
+    match /= len(writing_style.tag_counts)
+
+    return [(1 - abs((gl_writing_style.mean_stdev_word_length - writing_style.stdev_word_length) /
+                     gl_writing_style.mean_stdev_word_length)) * 100,
+
+            (1 - abs((gl_writing_style.mean_stdev_sentence_length - writing_style.stdev_sentence_length) /
+                     gl_writing_style.mean_stdev_sentence_length)) * 100,
+
+            match * 100]
 
 
 class WritingStyle:
@@ -51,9 +62,7 @@ class WritingStyle:
         self.tag_counts = self._get_tag_counts(text)
 
         # The writing style
-        self.average_word_length = average_word_lengths(text)
         self.stdev_word_length = stdev_word_lengths(text)
-        self.average_sentence_length = average_sentence_lengths(text)
         self.stdev_sentence_length = stdev_sentence_lengths(text)
 
     @staticmethod
@@ -85,24 +94,28 @@ class GeolocatedWritingStyle:
         # The mean writing-style
         self.count = 1
         self.tag_counts = Counter()
-        self.mean_average_word_length = writing_style.average_word_length
         self.mean_stdev_word_length = writing_style.stdev_word_length
-        self.mean_average_sentence_length = writing_style.stdev_word_length
         self.mean_stdev_sentence_length = writing_style.stdev_sentence_length
+
+    def get_mean_tags(self):
+        """
+        Calculates the mean counts of the tags and returns them
+        :return: The mean of the tags
+        """
+        tag_counts = deepcopy(self.tag_counts)
+
+        for key, value in tag_counts.items():
+            tag_counts[key] = float(value) / float(self.count)
+
+        return tag_counts
 
     def add_writing_style(self, writing_style):
         """
         Adds a writing-style.
         :param writing_style: The actual writing-style
         """
-        self.mean_average_word_length = (self.mean_average_word_length * self.count +
-                                         writing_style.average_word_length) / (self.count + 1)
-
         self.mean_stdev_word_length = (self.mean_stdev_word_length * self.count +
                                        writing_style.stdev_word_length) / (self.count + 1)
-
-        self.mean_average_sentence_length = (self.mean_average_sentence_length * self.count +
-                                             writing_style.average_sentence_length) / (self.count + 1)
 
         self.mean_stdev_sentence_length = (self.mean_stdev_sentence_length * self.count +
                                            writing_style.stdev_sentence_length) / (self.count + 1)

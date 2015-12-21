7386efc58dd15b9c98f6d7e9ea09a3e8f7819c49
Michael Hundt <michael.hundt@uni-konstanz.de>
2015-12-21 22:38:12 +0100

    twitter play

--- assignments/04/a4_4_live_twitter.py
+++ assignments/04/a4_4_live_twitter.py
@@ -1,9 +1,11 @@
-__author__ = 'Bassel Khatib & Michael Hundt'
-
-
+import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+
+__author__ = 'Bassel Khatib & Michael Hundt'
+
+
 
 
 consumer_key = "pVCg0oEa9s6wk6R8RpgYOpuok"
@@ -26,17 +28,39 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
+
+
+
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
+    # Construct the API instance
+    api = tweepy.API(auth)
+
+    # followers and friends ?
+    #friends = api.lookup_friendships(api)
+    # for user in tweepy.Cursor(api.followers, screen_name="twitter").items():
+    #     if user.geo_enabled:
+    #         print(user.screen_name + " - "+ str(user.followers_count) + " - ")
+
+    # for follower in tweepy.Cursor(api.followers).items():
+    #     follower.follow()
+
+    # tweets that I follow
+    # public_tweets = api.home_timeline()
+    # for tweet in public_tweets:
+    #     print(tweet.text)
+
+    # Open Stream
     stream = Stream(auth, l)
 
-    # take the 'random' sample data, which Twitter provide
-    stream.sample()
+    # take the 'random' english sample data, which Twitter provide
+    #stream.sample(languages=["en"])
 
     # Filter for keywords
     #stream.filter(track=['Love', 'air'])
 
     # Filter for location in WGS 84 projection
-    #stream.filter(locations= (-74,40,-73,41))  # New York
+    #stream.filter(locations= (-74,40,-73,41))      # New York
+    stream.filter(locations= (-180,-90,180,90), languages=["en"])     # whole World, english

a501f06f51488931babf1af8007f73055e067104
Lukas Eichkorn <lukas.eichkorn@uni-konstanz.de>
2016-01-13 09:05:30 +0100

    Removed some now unused files Renamed files

--- project01/utils/corpus_creator.py
+++ project01/utils/corpus_creator.py
@@ -7,10 +7,6 @@ from edit_extractor import Page, Revision
 __author__ = 'wikipedia_project_group'
 
 
-
-
-
-
 def read_pickles(input_dir, corpus_dir):
     train_files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
     country_id = 0
@@ -32,7 +28,6 @@ def read_pickles(input_dir, corpus_dir):
                 countries_id_dic[country] = country_id
                 country_id += 1
 
-
             if country == "":
                 continue
 
@@ -48,7 +43,6 @@ def read_pickles(input_dir, corpus_dir):
                     file.write(str(countries_id_dic[country]) + '\t' + revision.diff_content + '\n')
 
 
-
 if __name__ == '__main__':
-    corpora = read_pickles("../data/pickle/", "../data/corpora/countries/" )
+    read_pickles("../data/pickle/", "../data/countries/" )
 

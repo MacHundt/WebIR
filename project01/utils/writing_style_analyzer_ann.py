2990a891345d44d60d94d39d22295d5503581b18
Lukas Eichkorn <lukas.eichkorn@uni-konstanz.de>
2016-01-17 11:11:57 +0100

    Updated writing_style_analyzer_ann.py to fit the new writing_style_analyzer.py

--- project01/utils/writing_style_analyzer_ann.py
+++ project01/utils/writing_style_analyzer_ann.py
@@ -4,6 +4,8 @@ import numpy as np
 
 import nltk
 
+import pickle
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import FeatureUnion
@@ -23,7 +25,9 @@ from os.path import isfile, join
 
 def split_word_batches(filename, size_of_chunk=100):
     """
-    Open the file, remove mentions & links,  split it in  chunks of n words and return the n-word block
+    Open the file, remove mentions & links,  split it in  chunks of n words and return the n-word block.
+    :param filename: The name of the file
+    :param size_of_chunk: The chunk size
     """
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -48,38 +52,52 @@ def split_word_batches(filename, size_of_chunk=100):
         yield ' '.join(text[start:end])
 
 
-def load_corpus(input_dir):
-    train_files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
-    train_set = []
+def load_corpus(directory):
+    """
+    Loads the corpus (in this case countries) from within a given directory.
+    :param directory: The actual directory
+    :return: Returns the train-set
+    """
+    train_files = [f for f in listdir(directory) if isfile(join(directory, f))]
+    train_set = {}
 
     for f in train_files:
         if f == ".DS_Store":
             continue
 
+        train_set[f] = []
+
         label = f
-        df = pd.read_csv(input_dir + "/" + f, sep="\t", dtype={'id': object, 'text': object})
+        df = pd.read_csv(directory + "/" + f, sep="\t", dtype={'id': object, 'text': object})
 
         for row in df['text']:
             if type(row) is str:
-                train_set.append({"label": label, "text": row})
+                train_set[label].append({"label": label, "text": row})
 
     return train_set
 
 
-def pos_tags(txt):
-    tokens = nltk.word_tokenize(txt)
+def retrieve_pos_tags(text):
+    """
+    Retrieves the pos-tags of a text.
+    :param text: The actual text
+    :return: Returns the pos-tags of the given text
+    """
+    tokens = nltk.word_tokenize(text)
     return " ".join([tag for (word, tag) in nltk.pos_tag(tokens)])
 
 
 def train_model(train_set):
     """
     Train the models, using 10-fold-cv and LibLinear classification.
+    :param train_set: The set that is used for training
     """
     # Create two blocks of features, word anc character n-grams, size of 2
     # We can also append here multiple other features in general
     word_vector = TfidfVectorizer(analyzer="word", ngram_range=(2, 2), binary=False, max_features=2000)
     char_vector = TfidfVectorizer(ngram_range=(2, 3), analyzer="char", binary=False, min_df=0, max_features=2000)
-    tag_vector = TfidfVectorizer(analyzer="word", ngram_range=(2, 2), binary=False, max_features=2000, decode_error='ignore')
+    tag_vector = TfidfVectorizer(analyzer="word", ngram_range=(2, 2), binary=False, max_features=2000,
+                                 decode_error='ignore')
 
     # Our vectors are the feature union of word/char n-grams
     vectorizer = FeatureUnion([("chars", char_vector), ("words", word_vector)])
@@ -88,41 +106,60 @@ def train_model(train_set):
     corpus = []
     # Classes is the labels of each chunk
     classes = []
-    # Tags
+    # The tags of the n-word chunks
     tags = []
 
-    # Load training sets, for males & females
-    for item in train_set:
-        corpus.append(item['text'])
-        classes.append(item['label'])
-        tags.append(pos_tags(item['text']))
+    # Load training set
+    for key, country_list in train_set.items():
+        print("Processing " + '"' + key + '"')
 
-    print("size of corpus: " + str(sys.getsizeof(corpus)))
-    print("num of training instances: ", len(classes))
-    print("num of training classes: ", len(set(classes)))
+        for item in country_list:
+            corpus.append(item['text'])
+            classes.append(item['label'])
+            tags.append(retrieve_pos_tags(item['text']))
+
+    print()
+    print("Size of corpus: " + str(sys.getsizeof(corpus)))
+    print("Number of training instances: ", len(classes))
+    print("Number of training classes: ", len(set(classes)))
 
     # Fit the model of tf-idf vectors for the corpus
-    X1 = vectorizer.fit_transform(corpus)
-    X2 = tag_vector.fit_transform(tags)
+    x1 = vectorizer.fit_transform(corpus)
+    x2 = tag_vector.fit_transform(tags)
 
-    matrix = hstack((X1, X2), format='csr')
+    matrix = hstack((x1, x2), format='csr')
 
-    print("num of features: ", len(vectorizer.get_feature_names()))
-    print("training model")
+    print("Number of features: ", len(vectorizer.get_feature_names()))
 
     x = matrix.toarray()
     y = np.asarray(classes)
 
-    model = LinearSVC(loss='l1', dual=True)
+    print()
+    print("Training model")
+
+    model = LinearSVC(loss='hinge', dual=True)
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
-    y_prediction = model.fit(x_train, y_train).predict(x_test)
+    model.fit(x_train, y_train)
+    y_prediction = model.predict(x_test)
     cm = confusion_matrix(y_test, y_prediction)
 
-    print(cm)
+    print("Saving model")
 
-    fscore = (2 * cm[0][0]) / ((2 * cm[0][0]) + cm[1][0] + cm[0][1])
-    print("f-score: " + str(fscore))
+    pickle.dump(model, open('../data/model/trained_model', 'wb'))
+
+    # Calculate the accuracy
+    diagonal = 0
+    rest = 0
+    for i, v1 in enumerate(cm):
+        for j, v2 in enumerate(cm[i]):
+            if i == j:
+                diagonal += cm[i][j]
+            else:
+                rest += cm[i][j]
+
+    print()
+    print("Accuracy: " + str((1 - (rest / (diagonal + rest))) * 100) + '%')
 
     pl.matshow(cm)
     pl.title('Confusion matrix')
@@ -134,7 +171,5 @@ def train_model(train_set):
 
 if __name__ == '__main__':
     corpora = load_corpus("../data/countries")
-
-    print(len(corpora), corpora[0:10])
 
     train_model(corpora)

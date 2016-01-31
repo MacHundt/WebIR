f953d8b664ff33b3eaed352f881f9f90f529f87d
shokodmeon <boo.factor@gmail.com>
2016-01-31 02:25:54 +0100

    90% done

--- visualization/js/geoWookieViz.js
+++ visualization/js/geoWookieViz.js
@@ -1,187 +1,160 @@
-//Use data binding to construct a common html table
+//geoWookie visualization
 
-//Use data binding to construct a bar chart for each data point. Layout the single bar charts in a simple grid. There is no need for axes or text information.
-function billy() {
-  console.log("importing")
-  var filename = 'csv/result_stat.csv';
-//   d3.csv(filename, function(error, data) {
-//     data.forEach(function(d) {
-//         d.date = parseDate(d.Hour);
-//         d.T = +d.T;
-//     });
-// });
+// load file using file reader as blob
+// preprocess blob
+// turn blob into proper data format
+// feed to charts 
 
-   if (window.File && window.FileReader && window.FileList && window.Blob) {
-    // Great success! All the File APIs are supported.
-  } else {
-    alert('The File APIs are not fully supported in this browser.');
-  } 
-}
-  function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
+var pieChartBlob;
+var barChartBlob;
+var processedPieData = [];
+var processedBarData = [];
+var accuracy = "Not Set";
 
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
-    }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+function processPieBlob(text) {
+  //process accuracy first
+  var totLine = text[text.length-2].split(",");
+  accuracy = (totLine[2] / totLine[1])*100 ;
+  accuracy = accuracy.toString().substring(0,5).concat("%");
+
+  // continue to the rest
+  var startAt = 1;
+  var endAt = 7;
+  for (var i = startAt; i < endAt; i += 1) {
+    var line = text[i].split(",");
+    var countryName = line[0];
+    var truePositives = line[1];
+    console.log(line, countryName, truePositives);
+    processedPieData.push({
+      key: countryName,
+      y: parseInt(truePositives)
+    });
   }
-function findSize(file) {
-    var fileInput = file; 
-    try{
-        alert(fileInput.files[0].size); // Size returned in bytes.
-    }catch(e){
-        var objFSO = new ActiveXObject("Scripting.FileSystemObject");
-        var e = objFSO.getFile( fileInput.value);
-        var fileSize = e.size;
-        alert(fileSize);    
-    }
-}
+}  
 
-function processResultStat(data) {
-}
-function buildCharts() {
-      var piedata = [
-        {key: "One", y: 5},
-        {key: "Two", y: 2},
-        {key: "Three", y: 9},
-        {key: "Four", y: 7},
-        {key: "Five", y: 4},
-        {key: "Six", y: 3},
-        {key: "Seven", y: 0.5}
-    ];
-        var barchartdata =[
-  {
+function processChartBlob(text) {
+  var midRes = [];
+  var startAt = 1;
+  var endAt = text.length -1 ;
+  var counter = 0;
+  for (var i = startAt; i < endAt; i += 1) {
+    var line = text[i].split(",");
+    var countryName = line[0];
+    var bytes = line[1];
+    midRes.push({
+//      label: countryName,
+      label: counter,
+      // megabytes
+      value: parseInt(bytes)/1000000
+    });
+    counter++;
+  }
+  processedBarData.push({
     key: "Cumulative Return",
-    values: [
-      { 
-        "label" : "A" ,
-        "value" : -29.765957771107
-      } , 
-      { 
-        "label" : "B" , 
-        "value" : 0
-      } , 
-      { 
-        "label" : "C" , 
-        "value" : 32.807804682612
-      } , 
-      { 
-        "label" : "D" , 
-        "value" : 196.45946739256
-      } , 
-      { 
-        "label" : "E" ,
-        "value" : 0.19434030906893
-      } , 
-      { 
-        "label" : "F" , 
-        "value" : -98.079782601442
-      } , 
-      { 
-        "label" : "G" , 
-        "value" : -13.925743130903
-      } , 
-      { 
-        "label" : "H" , 
-        "value" : -5.1387322875705
-      }
-    ]
-  }
-]
- 
+    values:  midRes
+  });
+}
 
-            var height = 350;
+function readBlob(opt_startByte, opt_stopByte) {
+
+  var files = document.getElementById('files').files;
+  if (!files.length) {
+    alert('Please select a file!');
+    return;
+  }
+
+  var file1 = files[0];
+  var file2 = files[1];
+  var start1 = parseInt(opt_startByte) || 0;
+  var stop1 = parseInt(opt_stopByte) || file1.size - 1;
+  var start2 = parseInt(opt_startByte) || 0;
+  var stop2 = parseInt(opt_stopByte) || file2.size - 1;
+  var reader1 = new FileReader();
+  var reader2 = new FileReader();
+  console.log(file1, file2, start1, stop1, start2, stop2)
+
+    // If we use onloadend, we need to check the readyState.
+
+    reader1.onloadend = function(evt) {
+      if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        pieChartBlob = evt.target.result.split("\n");
+        processPieBlob(pieChartBlob);
+        document.getElementById('byte_content').textContent = evt.target.result;
+        document.getElementById('byte_range').textContent = 
+        ['Read bytes: ', start1 + 1, ' - ', stop1 + 1,
+        ' of ', file1.size, ' byte file'].join('');
+      }
+    };
+    reader2.onloadend = function(evt) {
+      if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        barChartBlob = evt.target.result.split("\n");
+        processChartBlob(barChartBlob);
+        document.getElementById('byte_content').textContent = evt.target.result;
+        document.getElementById('byte_range').textContent = 
+        ['Read bytes: ', start2 + 1, ' - ', stop2 + 1,
+        ' of ', file2.size, ' byte file'].join('');
+      }
+    };
+    blob1 = file1.slice(start1, stop1 + 1);
+    blob2 = file2.slice(start2, stop2 + 1);
+    var barChartData = reader1.readAsText(blob1);
+    var pieChartData = reader2.readAsText(blob2);
+  }
+
+
+  function buildCharts(pieChartData, barChartData) {
+    var piedata = processedPieData;
+    var barchartdata = processedBarData;
+
+
+    var height = 350;
     var width = 450;
     var chart1;
+
+    //pie chart 
     nv.addGraph(function() {
-        var chart1 = nv.models.pieChart()
-            .x(function(d) { return d.key })
-            .y(function(d) { return d.y })
-            .donut(true)
-            .width(width)
-            .height(height)
-            .padAngle(.08)
-            .cornerRadius(10)
+      var chart1 = nv.models.pieChart()
+      .x(function(d) { return d.key })
+      .y(function(d) { return d.y })
+      .donut(true)
+      .width(width)
+      .height(height)
+      .padAngle(.08)
+      .cornerRadius(10)
             .id('donut1'); // allow custom CSS for this one svg
-        chart1.title("Results");
-        chart1.pie.donutLabelsOutside(true).donut(true);
-        d3.select("#piechart")
+            chart1.title(accuracy);
+            chart1.pie.donutLabelsOutside(true).donut(true);
+            d3.select("#piechart")
             .datum(piedata)
             .transition().duration(1200)
             .call(chart1);
-        return chart1;
-    });
-nv.addGraph(function() {
-  var chart = nv.models.discreteBarChart()
-    .x(function(d) { return d.label })
-    .y(function(d) { return d.value })
-    .staggerLabels(true)
+            return chart1;
+          });
+
+//bar chart
+    nv.addGraph(function() {
+      var chart = nv.models.discreteBarChart()
+      .x(function(d) { return d.label })
+      .y(function(d) { return d.value })
+//      .staggerLabels(true)
 //    .tooltips(false)
-    .showValues(true)
+.showValues(true)
 
-  d3.select('#barchart')
-    .datum(barchartdata)
-    .transition().duration(500)
-    .call(chart)
-    ;
+d3.select('#barchart')
+.datum(barchartdata)
+.transition().duration(500)
+.call(chart)
+;
 
-  nv.utils.windowResize(chart.update);
+nv.utils.windowResize(chart.update);
 
-  return chart;
+return chart;
 });
-
-
-}
-// function buildPieChart(dataPoints) {
-//         'use strict';
-
-//         var dataset = [
-//           { label: 'Abulia', count: 10 }, 
-//           { label: 'Betelgeuse', count: 20 },
-//           { label: 'Cantaloupe', count: 30 },
-//           { label: 'Dijkstra', count: 40 }
-//         ];
-
-//         var width = 360;
-//         var height = 360;
-//         var radius = Math.min(width, height) / 2;
-
-//         var color = d3.scale.category20b();
-
-//         var svg = d3.select('#chart')
-//           .append('svg')
-//           .attr('width', width)
-//           .attr('height', height)
-//           .append('g')
-//           .attr('transform', 'translate(' + (width / 2) + 
-//             ',' + (height / 2) + ')');
-
-//         var arc = d3.svg.arc()
-//           .outerRadius(radius);
-
-//         var pie = d3.layout.pie()
-//         //   .value(function(d) { return d.count; })
-//         //   .sort(null);
-
-//         // var path = svg.selectAll('path')
-//         //   .data(pie(dataset))
-//         //   .enter()
-//         //   .append('path')
-//         //   .attr('d', arc)
-//         //   .attr('fill', function(d, i) { 
-//         //     return color(d.data.label);
-//         // });
-// }
+  }
 
 function start()
 {                
     buildCharts();
-    billy();
     console.log("==========");
 }
 

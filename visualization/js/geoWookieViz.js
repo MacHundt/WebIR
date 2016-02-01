af0f321b9c8914c7aa0c4898fc5a613ae90bccba
shokodmeon <boo.factor@gmail.com>
2016-02-01 15:38:34 +0100

    cleanup

--- visualization/js/geoWookieViz.js
+++ visualization/js/geoWookieViz.js
@@ -2,53 +2,50 @@
 
 // load file using file reader as blob
 // preprocess blob
-// turn blob into proper data format
-// feed to charts 
+// feed to charts nom nom nom
+// - there's an issue here; the pie chart seems to fail to init sometimes. 
+// - tried to fix but it still fails. cause unknown. clicking visualize again
+// - seems to load the data properly.
 
+
+
+// the blobs (file dumps) and the data 
+// (processed file dumps) go into these babies
 var pieChartBlob;
 var barChartBlob;
 var processedPieData = [];
 var processedBarData = [];
 var accuracy = "Not Set";
-var sliceSizeMax = 7;
 
-// function sortNumber(a,b) {
-//    return a - b;
-// }
-// function sortJsObject(object) {
-//     var dict = object;
+// how many results do we want to visualize
+var sliceSizeMax = 5;
+// after splitting the array on spaces, 
+// the first entry is the header
+// so we skip the text[0] and start at text[1]
+var startAt = 1;
+// n is [], n-1 is Total field - needed to calc. accuracy.
+var skipEnd = 2;
 
-//     var keys = [];
-//     for(var key in dict) { 
-//        keys[keys.length] = key;
-//      }
-
-//      var values = [];     
-//      for(var i = 0; i < keys.length; i++) {
-//          values[values.length] = dict[keys [i]];
-//      }
-//      var sortedValues = values.sort(sortNumber);
-//      console.log(sortedValues);
-//      return sortedValues;
-// }
 
 // this is needed to sort values as integers
 function processPieBlob(text) {
   //process accuracy first
 
   var totLine = text[text.length-2].split(",");
+  console.log(JSON.stringify(text));
   var temp = [];
   accuracy = (totLine[2] / totLine[1])*100 ;
   accuracy = accuracy.toString().substring(0,5).concat("%");
 
   // continue to the rest
-  var startAt = 1;
   var endAt = text.length-2;
   for (var i = startAt; i < endAt; i += 1) {
     var line = text[i].split(",");
     var countryName = line[0];
-    var truePositives = line[1];
-//    console.log(line, countryName, truePositives);
+    // line[1] is retrieved
+    // line[0] is true positives 
+    var truePositives = line[2];
+    // console.log(line, "=>" ,countryName, truePositives);
     temp.push({
       key: countryName,
       y: parseInt(truePositives)
@@ -56,16 +53,28 @@ function processPieBlob(text) {
   }
 
 //  processedPieData = temp;
-  processedPieData = temp.sort(function(a,b){return a["y"] - b["y"];}).reverse().slice(0,sliceSizeMax); 
-  console.log(processedPieData)
+  processedPieData = temp.sort(
+    function(a,b) {
+      // pie array has lable: and y:
+      return a["y"] - b["y"];
+    })
+  .reverse()
+  .slice(0,sliceSizeMax); 
+  //console.log(processedPieData)
   }
 
 function processChartBlob(text) {
+  // midRes stores single json object 
   var midRes = [];
-  var startAt = 1;
-  var endAt = text.length -1 ;
+  // need a different end at for the file size csv - n is []
+  var endAt = text.length - 1 ;
   var counter = 0;
+  // generate single json object and append/push it to processedBarData
+  // one at a time
   for (var i = startAt; i < endAt; i += 1) {
+    // text[i] is a strong split at "\n"
+    // so we split using "," to get a 2-dim array
+    // with text[0] = country and text[1] = byte count
     var line = text[i].split(",");
     var countryName = line[0];
     var bytes = line[1];
@@ -86,9 +95,9 @@ function processChartBlob(text) {
 function readBlob(opt_startByte, opt_stopByte) {
 
   var files = document.getElementById('files').files;
-  if (!files.length) {
-    alert('Please select a file!');
-    return;
+  if (files.length > 2) {
+    alert('Please select BOTH files!');
+    return -1;
   }
 
   var file1 = files[0];
@@ -100,11 +109,12 @@ function readBlob(opt_startByte, opt_stopByte) {
   var reader1 = new FileReader();
   var reader2 = new FileReader();
 //  console.log(file1, file2, start1, stop1, start2, stop2)
-
-    // If we use onloadend, we need to check the readyState.
+// If we use onloadend, we need to check the readyState.
 
     reader1.onloadend = function(evt) {
       if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+
+        // splitting at line end to make our processors` life easier
         pieChartBlob = evt.target.result.split("\n");
         processPieBlob(pieChartBlob);
         document.getElementById('byte_content').textContent = evt.target.result;
@@ -115,14 +125,18 @@ function readBlob(opt_startByte, opt_stopByte) {
     };
     reader2.onloadend = function(evt) {
       if (evt.target.readyState == FileReader.DONE) { // DONE == 2
+        // splitting at line end to make our processors` life easier
         barChartBlob = evt.target.result.split("\n");
+        // process chart Blob here
         processChartBlob(barChartBlob);
-        document.getElementById('byte_content').textContent = evt.target.result;
-        document.getElementById('byte_range').textContent = 
-        ['Read bytes: ', start2 + 1, ' - ', stop2 + 1,
-        ' of ', file2.size, ' byte file'].join('');
+        // keeping this bit of code incase we one day want to dump the bytes file
+        // document.getElementById('byte_content').textContent = evt.target.result;
+        // document.getElementById('byte_range').textContent = 
+        // ['Read bytes: ', start2 + 1, ' - ', stop2 + 1,
+        // ' of ', file2.size, ' byte file'].join('');
       }
     };
+    // blobs and data holders 
     blob2 = file2.slice(start2, stop2 + 1);
     blob1 = file1.slice(start1, stop1 + 1);
     var pieChartData = reader2.readAsText(blob2);
@@ -131,13 +145,12 @@ function readBlob(opt_startByte, opt_stopByte) {
 
 
   function buildCharts() {
-//    console.log(processedPieData)
     var piedata = processedPieData;
     var barchartdata = processedBarData;
 
 
-    var height = 350;
-    var width = 450;
+    var height = 450;
+    var width = 550;
     var chart1;
 
     //pie chart 
